dff67296325672ab426bf20529f031fb857f7d4d
hustducnv <hust.ducnv@gmail.com>
2020-05-10 15:50:26 +0700

    Chocoe.java

--- src/Choco2.java
+++ src/Choco2.java
@@ -1,24 +1,24 @@
-import org.chocosolver.solver.Model;
-import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.variables.IntVar;
-
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Choco {
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.variables.IntVar;
 
 
+public class Choco2 {
     int N; //so mon
     int M; // so phong
     int[] d; // d[i] so luong sinh vien dang ki mon i
     int[] c; // c[i] suc chua cua phong i
     int[][] conflict; //conflict[i][j] : mon i va j khong the cung kip
+    int[] oneM;
 
     public static void main(String[] args) {
-        Choco app = new Choco();
+        Choco2 app = new Choco2();
         app.input("data/input1.txt");
         app.solve();
     }
@@ -58,89 +58,98 @@ public class Choco {
         }
     }
 
-    /**
-     * 
-     */
     public void solve() {
-        // check xem co mon nao qua nhieu sinh  vien,  khong the thi o bat ki phong nao
-        //
+        // check xem co mon nao qua nhieu sinh  vien,
+        oneM = new int[M];
+        for (int i = 0; i < M ; i++) {
+            oneM[i] = 1;
+        }
 
         Model model = new Model();
-        IntVar[] X = model.intVarArray(N, 1, N);  //X[i]  kip thi cua mon i
-        IntVar[] Y = new IntVar[N];  // Y[i] phong thi cua mon i
+        IntVar[] X = model.intVarArray(N, 1, N);
+        IntVar[][] Y = model.intVarMatrix(N, M, 0, 1);
+        IntVar Z = model.intVar(1, N);
+
+        //constraints
+        //1.
         for (int i = 0; i < N; i++) {
-            // luu cac phong co suc chua c[j] >=  d[i] so sinh vien mon i
-            ArrayList<Integer> tlist = new ArrayList<>();
-            for (int j = 0; j < M; j++) {
-                if (c[j] >= d[i]) {
-                    tlist.add(j+1);
-                }
-            }
-            int[] tarr = new int[tlist.size()];
-            for (int v = 0; v < tlist.size();  v++) tarr[v] = tlist.get(v);
-            Y[i] = model.intVar("y" + i, tarr);
+            model.scalar(Y[i], oneM, ">=", 1).post();
         }
 
-        IntVar Z = model.intVar("z", 1, N);
-
-        //constrain
-        //1. not conflict (i, j) conflict => X[i] != X[j]
-        for (int i = 0; i < N-1; i++) {
-            for (int j = i+1; j < N; j++) {
-                if (conflict[i][j] == 1) {
-                    model.arithm(X[i], "!=", X[j]).post();
+        //2.
+        for (int i1 = 0; i1 < N-1; i1++) {
+            for (int i2 = i1+1; i2 < N; i2++) {
+                if (conflict[i1][i2] == 1) {
+                    model.arithm(X[i1], "!=", X[i2]).post();
                 }
             }
         }
 
-        //2.suc chua >= so sinh vien: da the hien o phan khai bao bien Y[i]
+        //3
+        for (int i1 = 0; i1 < N-1; i1++) {
+            for (int i2 = i1+1; i2 < N; i2++) {
+                for (int j = 0; j < M; j++) {
+                    model.ifThen(
+                    		model.arithm(X[i1], "=", X[i2]),
+                            model.arithm(Y[i1][j], "+", Y[i2][j], "<=", 1)
+                    );
+                }
 
-        //3. mon i, j cung kip => khac phong, hoac nguoc lai
-        // if X[i] = X[j] => Y[i] != Y[j]
-        for (int i = 0; i < N-1; i++) {
-            for (int j = i+1; j < N; j++) {
-                model.ifThen(
-                    model.arithm(X[i], "=", X[j]),
-                    model.arithm(Y[i], "!=", Y[j])
-                );
             }
         }
 
-        //4. Z >= X[i]
+        //4
         for (int i = 0; i < N; i++) {
-            model.arithm(Z, ">=", X[i]).post();
+            model.scalar(Y[i], c, ">=", d[i]).post();
         }
 
-        model.setObjective(Model.MINIMIZE, Z);
-       
-        
+        //5
+        for (int i = 0; i < N; i++) {
+            model.arithm(Z , ">=", X[i]).post();
+        }
+
+        model.setObjective(false, Z);
 
         Solver solver = model.getSolver();
-        
 
         while (solver.solve()) {
             //stop when find an optimal solution
-            System.out.println("---------------------------");
+            System.out.println("\n---------------------------");
             System.out.println("objective value: " + solver.getBestSolutionValue());
             for (int i = 0; i < N; i++) {
-                System.out.println(i +  ". " +  X[i].getValue() + " " + Y[i].getValue());
+                System.out.println();
+                System.out.print("Mon " + i  + " kip " + X[i].getValue() + ", phong: ");
+                for (int j = 0; j < M; j++) {
+                    if (Y[i][j].getValue() == 1) {
+                        System.out.print(j + ", ");
+                    }
+                }
             }
         }
-        
 
-//        Find All Solutions
-        
+        //Find All Solutions
 //        List<Solution> solutions = solver.findAllOptimalSolutions(Z, false);
 //        System.out.println("number of optimal solutions: " + solutions.size());
 //        for (Solution s : solutions) {
 //            System.out.println("---------------------------");
 //            System.out.println("optimal objective value: " + s.getIntVal(Z));
+//
 //            for (int i = 0; i < N; i++) {
-//                System.out.println(i +  ". " +  s.getIntVal(X[i]) + " " + s.getIntVal(Y[i]));
+//                System.out.println();
+//                System.out.print("Mon " + i  + " kip " + s.getIntVal(X[i]) + ", phong: ");
+//                for (int j = 0; j < M; j++) {
+//                    if (s.getIntVal(Y[i][j])== 1) {
+//                        System.out.print(j + ", ");
+//                    }
+//                }
 //            }
+//
 //        }
-        
+
         solver.printStatistics();
+
+
+
 
 
     }
---
+++ src/RanData.java
@@ -0,0 +1,9 @@
+
+public class RanData {
+	public void ran() {
+		
+		
+		
+		
+	}
+}
